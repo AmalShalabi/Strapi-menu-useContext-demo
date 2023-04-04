404f307604434859d36a01a7014932339bc2b110
unknown <mouni.shalaby@gmail.com>
2023-04-04 11:20:16 +0300

    Navbar CSS

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,15 +1,17 @@
 import { FaBars } from "react-icons/fa";
-import {useGlobalContext} from "./context";
+import { useGlobalContext } from "./context";
 
 const Navbar = () => {
   const { openSidebar } = useGlobalContext();
   return (
-    <nav className="nav-center">
-      <h3 className="logo">strapi</h3>
-      <button className="toggle-btn" onClick={openSidebar}>
-        <FaBars />
-      </button>
-      {/* navlinks later */}
+    <nav>
+      <div className="nav-center">
+        <h3 className="logo">strapi</h3>
+        <button className="toggle-btn" onClick={openSidebar}>
+          <FaBars />
+        </button>
+        {/* navlinks later */}
+      </div>
     </nav>
   );
 };
