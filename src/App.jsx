7b8647364f7df58288c49e66f1c29a782d7d68c2
unknown <mouni.shalaby@gmail.com>
2023-04-04 10:02:53 +0300

    Global Context

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import { useGlobalContext } from "./context";
 
 const App = () => {
+  const { isSidebarOpen } = useGlobalContext();
+  console.log(isSidebarOpen);
   return (
     <main>
- <h1>strap starter</h1>
+      <h1>strap starter</h1>
     </main>
   );
 };
