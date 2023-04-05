a2ac5bb0d315e68374ee7ab80f0f49276131e2db
unknown <mouni.shalaby@gmail.com>
2023-04-05 10:14:23 +0300

    hide  submenu

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <div className="hero-container">
       <div className="hero-center">
         <h1>
-          Manage Any Contetnt Anywhere. <br />
+          Manage Any Content Anywhere. <br />
         </h1>
         <p>
           Strapi is the leading open-source headless CMS. It’s 100% JavaScript
